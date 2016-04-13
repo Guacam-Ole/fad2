ee60ad60c27fa3a13d97e7db639dd51bc1cc4590
Ole <ole@nocarrier.de>
2016-04-13 19:58:26 +0200

    Just playing around a bit

---
+++ fad2/Fad.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace fad2
+{
+    partial class Fad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cardSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fADSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apiDocumentationforDevelopersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.licenseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BackPicture = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(20, 60);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1148, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cardSettingsToolStripMenuItem,
+            this.fADSettingsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // cardSettingsToolStripMenuItem
+            // 
+            this.cardSettingsToolStripMenuItem.Name = "cardSettingsToolStripMenuItem";
+            this.cardSettingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cardSettingsToolStripMenuItem.Text = "Card Settings";
+            // 
+            // fADSettingsToolStripMenuItem
+            // 
+            this.fADSettingsToolStripMenuItem.Name = "fADSettingsToolStripMenuItem";
+            this.fADSettingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fADSettingsToolStripMenuItem.Text = "FAD Settings";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.apiDocumentationforDevelopersToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.aboutToolStripMenuItem,
+            this.licenseToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem2.Text = "Help";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // apiDocumentationforDevelopersToolStripMenuItem
+            // 
+            this.apiDocumentationforDevelopersToolStripMenuItem.Name = "apiDocumentationforDevelopersToolStripMenuItem";
+            this.apiDocumentationforDevelopersToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.apiDocumentationforDevelopersToolStripMenuItem.Text = "Api Documentation (for Developers)";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(262, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // licenseToolStripMenuItem
+            // 
+            this.licenseToolStripMenuItem.Name = "licenseToolStripMenuItem";
+            this.licenseToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.licenseToolStripMenuItem.Text = "License";
+            // 
+            // BackPicture
+            // 
+            this.BackPicture.BackColor = System.Drawing.Color.Transparent;
+            this.BackPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BackPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BackPicture.Location = new System.Drawing.Point(20, 84);
+            this.BackPicture.Name = "BackPicture";
+            this.BackPicture.Size = new System.Drawing.Size(1148, 512);
+            this.BackPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.BackPicture.TabIndex = 1;
+            this.BackPicture.TabStop = false;
+            // 
+            // Fad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 616);
+            this.Controls.Add(this.BackPicture);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Fad";
+            this.Text = "FlashAirDownloader";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cardSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fADSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apiDocumentationforDevelopersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem licenseToolStripMenuItem;
+        private System.Windows.Forms.PictureBox BackPicture;
+    }
+}
+
---
+++ fad2/Fad.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fad2
+{
+    public partial class Fad : MetroFramework.Forms.MetroForm
+    {
+        private List<string> _imageLoop;
+        private string _currentBackImage;
+        Timer _imageSwitchTimer;
+        Random _random = new Random();
+
+        public Fad()
+        {
+            InitializeComponent();
+            ImageSwitcher();
+        }
+
+        private void ImageSwitcher()
+        {
+            // TODO: Allow local Images
+            _imageLoop = Directory.GetFiles($"{Application.StartupPath}\\examplepix").ToList();
+            _imageSwitchTimer = new Timer();
+            _imageSwitchTimer.Interval = 10000;  // TODO: From Settings
+            _imageSwitchTimer.Tick += _imageSwitchTimer_Tick;
+            _imageSwitchTimer.Start();
+            ChangeImage();
+        }
+
+        private void _imageSwitchTimer_Tick(object sender, EventArgs e)
+        {
+            ChangeImage();
+        }
+
+        private void ChangeImage()
+        {
+            int randomImageId= _random.Next(_imageLoop.Count);
+            ChangeImage(_imageLoop[randomImageId]);
+        }
+
+        private Bitmap ResizedImage(string path)
+        {
+            int alpha = 150;
+            var bitmap = new Bitmap(path);
+            double aspectRatio =  bitmap.Width / (double)bitmap.Height;
+            int maxWidth = BackPicture.Width;
+            int maxHeight = BackPicture.Height;
+            int imageWidth = maxWidth;
+            int imageHeight = (int)(imageWidth / aspectRatio);
+            if (imageHeight<maxHeight)
+            {
+                imageHeight = maxHeight;
+                imageWidth = (int)(imageHeight * aspectRatio);
+            }
+
+            var image = new Bitmap(bitmap, new Size(imageWidth, imageHeight));
+
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                Pen pen = new Pen(Color.FromArgb(alpha, 255, 255, 255), image.Width);
+                g.DrawLine(pen, -1, -1, image.Width, image.Height);
+                g.Save();
+            }
+
+
+            return image;
+        }
+
+        private void ChangeImage(string path)
+        {
+            BackPicture.Image = ResizedImage(path);
+        }
+    }
+}
